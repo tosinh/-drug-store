e1345ecd7d2ed3468c087b9a9b039d56f0b2e8c3
tosinh <tosinh680@gmail.com>
2024-03-07 18:15:24 +0700

    product render

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,163 @@
+export const data = {
+    "products": [
+        {
+            "id": 'T1',
+            "name": 'Men vi sinh Bacillus clausii',
+            "price": '80000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/Probiotic/vi%20khu%E1%BA%A9n/Bacillus%20clausii%201%20kg%20s%E1%BB%ADa%20n%E1%BB%99i%20dung.jpg'
+        },
+        {
+            "id": 'T2',
+            "name": 'Men vi sinh Bacillus coagulans',
+            "price": '300000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/Probiotic/vi%20khu%E1%BA%A9n/Men%20vi%20sinh%20Bacillus%20coagulans%201%20kg.jpg'
+        },
+        {
+            "id": 'T3',
+            "name": 'Vacxin PARVO',
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/v%E1%BA%AFc%20xin/Ch%C3%B3%20m%C3%A8o/hop%20Vac%20xin%20Pavo.png'
+        },
+        {
+            "id": 'T4',
+            "name": "Vacxin dại chó mèo Rabiva",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/v%E1%BA%AFc%20xin/Ch%C3%B3%20m%C3%A8o/Rabiva.png'
+        },
+        {
+            "id": 'T5',
+            "name": "Hanvit-K&C",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/vn/uploads/HASP/hanvit%20kc.jpg'
+        },
+        {
+            "id": 'T6',
+            "name": "Hantril-100",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/vn/uploads/HASP/hantril-100.jpg'
+        },
+        {
+            "id": 'T7',
+            "name": "Halusep BGF",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/H%C3%B3a%20ch%E1%BA%A5t%20s%C3%A1t%20tr%C3%B9ng/HLS(1).jpg'
+        },
+        {
+            "id": 'T8',
+            "name": "HANPROCILLIN",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/Kh%C3%A1ng%20sinh/H%E1%BB%97n%20d%E1%BB%8Bch%20ti%C3%AAm/Hanprocillin.jpg'
+        },
+        {
+            "id": 'T9',
+            "name": "Vắc xin ND-IB",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/uploads/S%E1%BA%A3n%20ph%E1%BA%A9m/v%E1%BA%AFc%20xin/Gia%20c%E1%BA%A7m/ND-IB.png'
+        },
+        {
+            "id": 'T10',
+            "name": "HANMECTIN-100",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/vn/uploads/HASP/hanmectin-100.jpg'
+        },
+        {
+            "id": 'T11',
+            "name": "HAN-NE-SOL",
+            "price": '500000',
+            "url": 'https://hanvet.com.vn/vn/uploads/HASP/han-ne-sol%20100g.jpg'
+        },
+    ],
+    "danh_muc": [
+        {
+            "ten": "THUỐC KHÁNG SINH",
+            "loai": [
+                "Bột trộn thức ăn hoặc pha nước uống",
+                "Dung dịch uống, viên đặt, bôi, xịt",
+                "Bột pha tiêm",
+                "Dung dịch tiêm",
+                "Hỗn dịch tiêm"
+            ]
+        },
+        {
+            "ten": "THUỐC KÝ SINH TRÙNG",
+            "loai": [
+                "Dạng bôi, nhỏ",
+                "Dạng viên, bột uống, trộn thức ăn",
+                "Dạng tắm, phun sương, xịt ngoài da",
+                "Dạng uống",
+                "Dạng tiêm"
+            ]
+        },
+        {
+            "ten": "VẮC XIN",
+            "loai": [
+                "Vắc xin cho cá",
+                "Vắc xin cho Chó, mèo",
+                "Vắc xin cho gia cầm",
+                "Vắc xin cho lợn (heo)"
+            ]
+        },
+        {
+            "ten": "CHẾ PHẨM SINH HỌC",
+            "loai": [
+                "Vi sinh vật có lợi (probiotic)",
+                "Kháng thể"
+            ]
+        },
+        {
+            "ten": "HOOC MÔN ĐIỀU TIẾT SINH SẢN",
+            "loai": []
+        },
+        {
+            "ten": "VITAMIN & PREMIX",
+            "loai": [
+                "Nguyên liệu san lẻ",
+                "Dạng bột uống, trộn thức ăn",
+                "Dạng dung dịch tiêm, uống",
+            ]
+        },
+        {
+            "ten": "HÓA CHẤT SÁT TRÙNG, KHỬ KHUẨN",
+            "loai": [
+                "DẠNG BỘT",
+                "DẠNG DUNG DỊCH"
+            ]
+        },
+        {
+            "ten": "HÓA CHẤT DIỆT CÔN TRÙNG",
+            "loai": []
+        },
+        {
+            "ten": "THUỐC DÙNG TRONG THỦY SẢN",
+            "loai": [
+                "CHẾ PHẨM BỔ SUNG THỨC ĂN",
+                "CHẾ PHẨM XỬ LÝ AO HỒ",
+            ]
+        },
+        {
+            "ten": "THUỐC KHÁC",
+            "loai": [
+                "Kháng viêm, giải độc, hạ sốt, giảm đau",
+                "Đạm sữa",
+                "Cấp cứu sốc phản vệ, ngưng tim, hôn mê"
+            ]
+        },
+        {
+            "ten": "CHẾ PHẨM SÁT KHUẨN TAY DÙNG TRONG GIA DỤNG VÀ Y TẾ",
+            "loai": []
+        },
+        {
+            "ten": "SẢN PHẨM CHÓ MÈO",
+            "loai": []
+        },
+        {
+            "ten": "KINH NGHIỆM",
+            "loai": [
+                "Kỹ thuật nuôi",
+                "Bệnh và điều trị"
+            ]
+        }
+    ]
+};
+
+export default data;
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import data from './Products.js'
 import './Products.css'
-export const Products = ({ title, items }) =>
+
+
+
+export const Products = () =>
 {
+    const { products, danh_muc } = data;
 
     return (
         <div className='products'>
@@ -9,47 +14,32 @@ export const Products = ({ title, items }) =>
                 <div className="row">
                     <div className="col-3">
                         <div className="category">
-
-                            <span>THUỐC KHÁNG SINH</span>
-                            <ul>
-                                <li>Bột trộn thức ăn hoặc pha nước uống</li>
-                                <li>Dung dịch uống, viên đặt, bôi, xịt</li>
-                                <li>Bột pha tiêm</li>
-                                <li>Dung dịch tiêm</li>
-                                <li>Hỗn dịch tiêm</li>
-                            </ul>
-                            <span>THUỐC KÝ SINH TRÙNG</span>
-                            <ul>
-                                <li>Dạng bôi, nhỏ</li>
-                                <li>Dạng viên, bột uống, trộn thức ăn</li>
-                                <li>Dạng tắm, phun sương, xịt ngoài da</li>
-                                <li>Dạng uống</li>
-                                <li>Dạng tiêm</li>
-                            </ul>
-                            <span>VẮC XIN</span>
-                            <ul>
-                                <li>Vắc xin cho cá</li>
-                                <li>Vắc xin cho Chó, mèo</li>
-                                <li>Vắc xin cho gia cầm</li>
-                                <li>Vắc xin cho lợn (heo)</li>
-                            </ul>
-                            <span>CHẾ PHẨM SINH HỌC</span>
-                            <ul>
-                                <li>Vi sinh vật có lợi (probiotic)</li>
-                                <li>Kháng thể</li>
-                            </ul>
+                            {data.danh_muc.map((danhMuc) => (
+                                <div key={danhMuc.ten} className="danh-muc">
+                                    <h6>{danhMuc.ten}</h6>
+                                    <ul>
+                                        {danhMuc.loai.map((loai) => (
+                                            <li key={loai}>{loai}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ))}
                         </div>
                     </div>
                     <div className="col-9">
-                        <div className="products">
-                            <div className="row">
-
-                            </div>
+                        <div className="product-list">
+                            {products.map((product) => (
+                                <div key={product.id} className="product-item">
+                                    <img src={product.url} alt={product.name} />
+                                    <span>{product.name}</span>
+                                    <p>{product.price} VND</p>
+                                    <button>Xem sản phẩm</button>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
